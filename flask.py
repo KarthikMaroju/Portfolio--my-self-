3a55a9006dc6e7a36882ffa000f6d371d8304e78
Maroju Karthik <karthikmaroju479@gmail.com>
2026-02-15 16:35:22 +0530

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    data = request.json
+    print(data)
+    return jsonify({"message":"Message received!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
